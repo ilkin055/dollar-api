17d0e2fe0a5e26dfeb6ac96d5f33d5278a90295e
ilkin055 <kerimlilkin055@gmail.com>
2024-05-19 00:48:22 +0400

    valut

--- script.js
+++ script.js
@@ -1,73 +1,223 @@
-let h3 = document.querySelector('h3')
-let li2 = document.querySelectorAll('.li2');
-let li3 = document.querySelectorAll('.li3');
-let p1 = document.querySelector('.p1')
-let p2 = document.querySelector('.p2')
+let currencies = ["RUB", "USD", "EUR", "AZN"];
+const apiKey = "fca_live_9LBhYW4At6eqnhTd5lV2PKmRkmZThVwmRqXvrWqk"
+class Calculator {
+    constructor(from, to, amount, rate, name) {
+        if (["first", "last"].indexOf(name) != -1) {
+            this.name = name;
+            this.setFrom(from);
+            this.setTo(to);
+            this.setAmount(amount);
+            this.setRate(rate);
+            this.amountConst = false;
+        }
+        else {
+            console.error("Invalid name");
+        }
+    }
+    setFrom(newFrom) {
+        if (currencies.indexOf(newFrom) != -1) {
+            this.from = newFrom;
+        }
+        else {
+            console.error("Invalid from");
+        }
+    }
+    setTo(newTo) {
+        if (currencies.indexOf(newTo) != -1) {
+            this.to = newTo;
+        }
+        else {
+            console.error("Invalid to");
+        }
+    }
+    setAmount(newAmount) {
+        if (typeof newAmount == "number") {
+            this.amount = Math.round(newAmount * 10000) / 10000;
+        }
+        else {
+            console.error("Invalid amount");
+        }
+    }
+    setRate(newRate) {
 
-let lftText = "1 RUB", rghtText = "USD", any;
-let base = 'RUB', symbols = 'USD';
+        if (typeof newRate == "number") {
+            this.rate = Math.round(newRate * 10000) / 10000;
+        }
+        else {
+            console.error("Invalid rate (line: 87)");
+        }
+    }
 
-// p1.innerHTML = lftText + " = " + any + rghtText;
-
-let input = document.querySelector('.inptget1')
-let output = document.querySelector('.inptget2')
-
-li2[0].style.backgroundColor = '#833AE0'
-li2[0].style.color = 'white'
-li3[1].style.backgroundColor = '#833AE0'
-li3[1].style.color = 'white'
-
-li2.forEach(item1 => {
-    item1.addEventListener('click', (event1) => {
-        event1.target.style.background = '#833AE0';
-        event1.target.style.color = 'white';
-        lftText = "1 " + event1.target.innerText;
-        base = event1.target.innerText;
-        myFunction();
-        li2.forEach(item2 => {
-            if (item2 != item1) {
-                item2.style.backgroundColor = 'white'
-                item2.style.color = '#C6C6C6'
+    show() {
+        let element = document.querySelector("." + this.name);
+        let buttons = [...element.children[1].children];
+        let input = element.children[2].children[0];
+        let buttom = element.children[2].children[1];
+        let clas = this.from.toLowerCase();
+        buttons.forEach(item => {
+            if (item.classList.contains(clas) && !item.classList.contains("selected")) {
+                item.classList.add("selected");
+            }
+            else if (!item.classList.contains(clas) && item.classList.contains("selected")) {
+                item.classList.remove("selected");
             }
         })
-    })
-})
+        if (!this.amountConst) {
+            input.value = this.amount;
+        }
+        buttom.innerHTML = `<span>1 ${this.from}</span> = <span>${this.rate} ${this.to}</span>`;
 
-li3.forEach(item3 => {
-    item3.addEventListener('click', (event2) => {
-        event2.target.style.backgroundColor = '#833AE0'
-        event2.target.style.color = 'white'
-        symbols = event2.target.innerText;
-        myFunction();
-        li3.forEach(item4 => {
-            if (item4 != item3) {
-                item4.style.backgroundColor = 'white'
-                item4.style.color = '#C6C6C6'
-            }
-        })
-    })
-})
-function myFunction() {
-    fetch(`https://v6.exchangerate-api.com/v6/f042570fb25bb227b790b7fb/latest/USD${base}&symbols=${symbols}`)     
-    .then(res => res.json())
-        .then(data => {
-            any = data.rates[symbols];
-            input.oninput = function () {
-                output.value = any.toFixed(4) * input.value.replaceAll(',', '.');
-            }
-            output.oninput = function () {
-                input.value = (1 / any).toFixed(4) * output.value.replaceAll(',', '.');
-            }
-            output.value = any.toFixed(4) * input.value.replaceAll(',', '.');
-            rghtText = any.toFixed(4) + " " + symbols;
-            p1.innerHTML = lftText + " = " + rghtText;
-            p2.innerHTML = "1 " + symbols + " = " + (1 / any).toFixed(4) + " " + base;
-        })
-        .catch(error => {
-            h3.textContent = error.message;
-            h3.style.display = 'block'
-        });
+    }
 
 }
 
-myFunction();
+class Calculators {
+    constructor(leftCurrency, rightCurrency, exchanges) {
+        if (currencies.indexOf(leftCurrency) != -1 && currencies.indexOf(rightCurrency) != -1) {
+            this.leftCurrency = leftCurrency;
+            this.rightCurrency = rightCurrency;
+            this.exchanges = exchanges;
+            this.left = new Calculator(leftCurrency, rightCurrency, 1, this.exchanges.getExchange(leftCurrency, rightCurrency), "first");
+            this.right = new Calculator(rightCurrency, leftCurrency, this.exchanges.getExchange(leftCurrency, rightCurrency), this.exchanges.getExchange(rightCurrency, leftCurrency), "last");
+            this.left.show();
+            this.right.show();
+        }
+        else {
+            console.error("Invalid currency");
+        }
+    }
+
+    changeLeftCurrency(newCurrency) {
+        this.leftCurrency = newCurrency;
+        this.left.setFrom(newCurrency);
+        this.left.setAmount(this.toFrom(this.right.amount));
+        this.left.setRate(this.exchanges.getExchange(this.leftCurrency, this.rightCurrency));
+        
+        this.right.setTo(this.leftCurrency);
+        this.right.setRate(this.exchanges.getExchange(this.rightCurrency, this.leftCurrency));
+        this.left.show();
+        this.right.show();
+    }
+    
+    changeRightCurrency(newCurrency) {
+        this.rightCurrency = newCurrency;
+        this.right.setFrom(newCurrency);
+        this.right.setAmount(this.left.amount * this.exchanges.getExchange(this.leftCurrency, this.rightCurrency));
+        this.right.setRate(this.exchanges.getExchange(this.rightCurrency, this.leftCurrency));
+
+        this.left.setTo(this.rightCurrency);
+        this.left.setRate(this.exchanges.getExchange(this.leftCurrency, this.rightCurrency));
+        this.left.show();
+        this.right.show();
+    }
+    
+    changeLeftAmount(amount) {
+        this.left.amountConst = true;
+        this.left.setAmount(amount);
+        this.right.setAmount(amount * this.exchanges.getExchange(this.leftCurrency, this.rightCurrency));
+
+        this.left.show();
+        this.right.show();
+        this.left.amountConst = false;
+    }
+
+    changeRightAmount(amount) {
+        this.right.amountConst = true;
+        this.right.setAmount(amount);
+        this.left.setAmount(this.toFrom(amount));
+
+        this.left.show();
+        this.right.show();
+        this.right.amountConst = false;
+    }
+
+    toFrom(amount) {
+        return amount * this.exchanges.getExchange(this.rightCurrency, this.leftCurrency);
+    }
+
+}
+
+
+const getDatas = async () => {
+    const response = await fetch(`https://api.currencyapi.com/v3/latest?apikey=${apiKey}&currencies=${currencies.join("%2C")}`);
+    const data = await response.json();
+
+    return data;
+}
+
+getDatas().then(data => {
+    let usdExchanges = {};
+    currencies.forEach(item => {
+        usdExchanges[item] = data.data[item].value;
+    })
+    let exchanges = {
+        usdExchanges: usdExchanges,
+        getExchange(from, to) {
+            if (currencies.indexOf(from) != -1 && currencies.indexOf(to) != -1) {
+                return this.usdExchanges[to] / this.usdExchanges[from];
+            }
+            else {
+                console.error("Invalid currency (line: 99)");
+            }
+        }
+    }
+
+    let calculators = new Calculators("RUB", "USD", exchanges);
+
+    document.querySelector(".first .rub").addEventListener("click", () => {
+        calculators.changeLeftCurrency("RUB");
+    })
+
+    document.querySelector(".first .usd").addEventListener("click", () => {
+        calculators.changeLeftCurrency("USD");
+    })
+
+    document.querySelector(".first .eur").addEventListener("click", () => {
+        calculators.changeLeftCurrency("EUR");
+    })
+
+    document.querySelector(".first .azn").addEventListener("click", () => {
+        calculators.changeLeftCurrency("AZN");
+    })
+
+    document.querySelector(".last .rub").addEventListener("click", () => {
+        calculators.changeRightCurrency("RUB");
+    })
+
+    document.querySelector(".last .usd").addEventListener("click", () => {
+        calculators.changeRightCurrency("USD");
+    })
+
+    document.querySelector(".last .eur").addEventListener("click", () => {
+        calculators.changeRightCurrency("EUR");
+    })
+
+    document.querySelector(".last .azn").addEventListener("click", () => {
+        calculators.changeRightCurrency("AZN");
+    })
+
+    document.querySelector(".first input").addEventListener("keyup", (event) => {
+        if (event.target.value.split("")[0] == "." || event.target.value.split("")[0] == "-") {
+            let arr = event.target.value.split("");
+            arr.shift();
+            event.target.value = Number(arr.join(""));
+        }
+        if (event.key != ".") {
+            calculators.changeLeftAmount(Number(event.target.value))
+        };
+    })
+
+    document.querySelector(".last input").addEventListener("keyup", (event) => {
+        if (event.target.value.split("")[0] == "." || event.target.value.split("")[0] == "-") {
+            let arr = event.target.value.split("");
+            arr.shift();
+            event.target.value = Number(arr.join(""));
+        }
+        if (event.key != ".") {
+            calculators.changeRightAmount(Number(event.target.value));
+        }
+    })
+
+}).catch(err => {
+    document.querySelector(".alert").classList.remove("display-none");
+})
